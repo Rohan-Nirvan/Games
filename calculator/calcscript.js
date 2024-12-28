b8ff04ba269436bcaf19fd8c00a8186e28cfa1e2
Rohan-Nirvan <rohannirvan31@gmail.com>
2024-12-28 15:37:31 +0530

    error function decimal and bracket + style update

--- calculator/calcscript.js
+++ calculator/calcscript.js
@@ -1,5 +1,12 @@
 const addButton = document.getElementById("add");
 const subButton = document.getElementById("sub");
+const multiplyButton = document.getElementById("multiply");
+const divideButton = document.getElementById("divide");
+//const percentageButton = document.getElementById("percentage");
+const decimalButton = document.getElementById("decimal");
+//const cButton = document.getElementById("c");
+const bracketleftButton = document.getElementById("bracket-left");
+const bracketrightButton = document.getElementById("bracket-right");
 const equalButton = document.getElementById("equal");
 let display = document.getElementById("display");
 
@@ -49,7 +56,21 @@ addButton.addEventListener("click", displaynew1);
 
 subButton.addEventListener("click", displaynew2);
 
-equalButton.addEventListener("click", displaynew3);
+multiplyButton.addEventListener("click", displaynew3);
+
+divideButton.addEventListener("click", displaynew4);
+
+//percentageButton.addEventListener("click", displaynew5);
+
+decimalButton.addEventListener("click", displaynew6);
+
+//cButton.addEventListener("click", displaynew7);
+
+bracketleftButton.addEventListener("click", displaynew8);
+
+bracketrightButton.addEventListener("click", displaynew9);
+
+equalButton.addEventListener("click", displaynew10);
 
 function displaynew1(event) {
   display.innerText += addButton.innerText;
@@ -59,16 +80,47 @@ function displaynew2(event) {
   display.innerText += subButton.innerText;
 }
 
-//function displaynew3(event) {
-  //display.innerText += equalButton.innerText;
-//  equal = ` Number( ${display.innerText})`;
-//
-//return display.innerText = equal;
-//}
-
 function displaynew3(event) {
+  display.innerText += multiplyButton.innerText;
+}
 
+function displaynew4(event) {
+  display.innerText += divideButton.innerText;
+}
+
+
+/*function displaynew5(event) {
+  display.innerText += percentageButton.innerText;
+}*/
+
+function displaynew6(event) {
+  display.innerText += decimalButton.innerText;
+}
+
+/*function displaynew7(event) {
+  display.innerText === cButton.innerText;
+}*/
+
+function displaynew8(event) {
+  display.innerText += bracketleftButton.innerText;
+}
+
+function displaynew9(event) {
+  display.innerText += bracketrightButton.innerText;
+}
+/*function displaynew3(event) {
+  display.innerText += equalButton.innerText;
+  equal = ` Number( ${display.innerText})`;
+
+return display.innerText = equal;
+}*/
+
+function displaynew10(event) {
+try {
     const result = eval(display.innerText);
     display.innerText = result;
+} catch (error) {
+  display.innerText = "Error";
+}
 
 }
